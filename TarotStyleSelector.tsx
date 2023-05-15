4b514928a3cb7ccff88cff5aa3f7f92cc37a0b4a
tf00185077 <88528914+tf00185077@users.noreply.github.com>
2023-05-15 09:12:01 +0800

    add new telling area 3 styles

--- TarotStyleSelector.tsx
+++ TarotStyleSelector.tsx
@@ -9,9 +9,10 @@ const TarotStyleSelector: React.FC = () => {
     ) => {
         const inputElement = event.target as HTMLButtonElement;
         console.log('now style', inputElement.value);
+        setSelect(inputElement.value)
     };
     return (
-        <div>
+        <div className='w-full flex-col'>
 
        
         <div className="flex w-full justify-around my-12 text-white font-semibold">
@@ -40,11 +41,58 @@ const TarotStyleSelector: React.FC = () => {
                 Small Cross
             </button>
         </div>
+        <div className='mx-auto w-full flex justify-around'>
+
             {
                 select === 'oneCard' &&
                 <div className='w-40 h-72 border-2 '></div>
                 
             }
+            {
+                select === 'threeCards' &&
+                <>
+                    <div className='w-40 h-72 border-2 '></div>
+                    <div className='w-40 h-72 border-2 '></div>
+                    <div className='w-40 h-72 border-2 '></div>
+                </>
+                
+            }
+     {
+    select === 'smallCross' && (
+        <div className='grid grid-cols-12'>
+            <div className='grid grid-cols-3'>
+                <div>X</div>
+                <div className='w-40 h-72 border-2'></div>
+                <div>O</div>
+            </div>
+            <div className='grid grid-cols-3'>
+                <div className='w-40 h-72 border-2'></div>
+                <div>X</div>
+                <div className='w-40 h-72 border-2'></div>
+            </div>
+            <div className='grid grid-cols-3'>
+                <div>O</div>
+                <div className='w-40 h-72 border-2'></div>
+                <div>X</div>
+            </div>
+            <div className='grid grid-cols-3'>
+                <div className='w-40 h-72 border-2'></div>
+                <div>O</div>
+                <div className='w-40 h-72 border-2'></div>
+            </div>
+            <div className='grid grid-cols-3'>
+                <div>X</div>
+                <div className='w-40 h-72 border-2'></div>
+                <div className='w-40 h-72 border-2'></div>
+            </div>
+            <div></div>
+        </div>
+    )
+}
+
+
+                
+            </div>
         </div>
     );
 };
